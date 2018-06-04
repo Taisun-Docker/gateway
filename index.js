7e33dbd0196e691d5c1d316485e08d74a79c94fe
thelamer <ryankuba@gmail.com>
2018-06-03 17:11:54 -0700

    taisun#61 adding a public port range for requests that do not require authentication

--- index.js
+++ index.js
@@ -17,66 +17,77 @@ var certificate = fs.readFileSync(lecertpath).toString();
 // Proxy server
 var proxyServer = https.createServer({key: privateKey,cert: certificate}, function (req, res) {
   var subdomain = req.headers.host.split('.')[0];
-  // Check for auth
-  if (auth(req) == true){
-    var parsed = url.parse(req.url,true);
-    if (subdomain == 'taisun-gateway'){
-      var safeSuffix = parsed.pathname.replace(/^(\.\.[\/\\])+/, '');
-      if (safeSuffix == '/' ){
-        var safeSuffix = '/public/index.html';
-      }
-      var fileLoc = path.join(__dirname, safeSuffix);
-      var filename = parsed.pathname.substring(parsed.pathname.lastIndexOf('/')+1);
-      fs.readFile(fileLoc, function(err, data) {
-        if (err) {
-          res.writeHead(404, 'Not Found');
-          res.write('404: File Not Found!');
-          return res.end();
+  // Bypass auth if this is a public port
+  if ( parseInt(subdomain) >= 49050 && parseInt(subdomain) <= 49150){
+    proxyrequest(subdomain);
+  }
+  else {
+    // Check for auth
+    if (auth(req) == true){
+      var parsed = url.parse(req.url,true);
+      if (subdomain == 'taisun-gateway'){
+        var safeSuffix = parsed.pathname.replace(/^(\.\.[\/\\])+/, '');
+        if (safeSuffix == '/' ){
+          var safeSuffix = '/public/index.html';
         }
-        else if (filename == 'gateway.js'){
-          var options = {
-            host: ip,
-            port: taisunport,
-            path: '/containers'
-          };
-          http.get(options, function(taisunres) {
-            var body = '';
-            taisunres.on('data', function(chunk) {
-              body += chunk;
-            });
-            taisunres.on('end', function() {
-              var templated = data.toString('utf8').replace('%%%CONTAINERS%%%',body).replace('%%%TAISUNPORT%%%',taisunport).replace('%%%TAISUNAUTH%%%',pass);
-              res.statusCode = 200;
-              res.write(templated);
+        var fileLoc = path.join(__dirname, safeSuffix);
+        var filename = parsed.pathname.substring(parsed.pathname.lastIndexOf('/')+1);
+        fs.readFile(fileLoc, function(err, data) {
+          if (err) {
+            res.writeHead(404, 'Not Found');
+            res.write('404: File Not Found!');
+            return res.end();
+          }
+          else if (filename == 'gateway.js'){
+            var options = {
+              host: ip,
+              port: taisunport,
+              path: '/containers'
+            };
+            http.get(options, function(taisunres) {
+              var body = '';
+              taisunres.on('data', function(chunk) {
+                body += chunk;
+              });
+              taisunres.on('end', function() {
+                var templated = data.toString('utf8').replace('%%%CONTAINERS%%%',body).replace('%%%TAISUNPORT%%%',taisunport).replace('%%%TAISUNAUTH%%%',pass);
+                res.statusCode = 200;
+                res.write(templated);
+                return res.end();
+              });
+            }).on('error', function(e) {
+              res.statusCode = 500;
+              res.write('Error connecting to taisun');
               return res.end();
             });
-          }).on('error', function(e) {
-            res.statusCode = 500;
-            res.write('Error connecting to taisun');
+          }
+          else{
+            res.statusCode = 200;
+            res.write(data);
             return res.end();
-          }); 
-        }
-        else{
-          res.statusCode = 200;
-          res.write(data);
-          return res.end();          
-        }
-      });
+          }
+        });
+      }
+      else{
+        proxyrequest(subdomain);
+      }
     }
     else{
-      var endpoint = 'http://' + ip + ':' + subdomain;
-      proxy.web(req, res, {target: endpoint}, function(e) {
-        res.statusCode = 200;
-        res.write(JSON.stringify(e));
-        return res.end();
-      });
+      res.statusCode = 401;
+      res.setHeader('WWW-Authenticate', 'Basic realm="Login"');
+      res.end('<html><body>You shall not pass</body></html>');
     }
   }
-  else{
-    res.statusCode = 401;
-    res.setHeader('WWW-Authenticate', 'Basic realm="Login"');
-    res.end('<html><body>You shall not pass</body></html>');
-  }
+
+function proxyrequest(port){
+  var endpoint = 'http://' + ip + ':' + port;
+  proxy.web(req, res, {target: endpoint}, function(e) {
+    res.statusCode = 200;
+    res.write(JSON.stringify(e));
+    return res.end();
+  });
+}
+
 });
 
 // Handle websocket upgrades
@@ -118,7 +129,7 @@ function auth(req){
     else {
       return false;
     }
-  }  
+  }
 }
 
 // Launch server
